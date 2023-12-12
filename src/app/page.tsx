05aadc70003143ed652a0cb1999ad1ce1076baef
mechaneyes <ray@mechaneyes.com>
2023-12-12 10:13:20 -0800

    separate hero images ° massive styling work
    ° broke out hero portrait v landscape images
    ° massive sass styling work
    ° mixins: breakpoints and fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,13 +34,25 @@ export default function Chat() {
               : ""
           }`}
         >
-          <Image
-            src="/images/hero--steve-johnson-unsplash.jpg"
-            alt="Generative Illustration by Steve Johnson on Unsplash"
-            width={828}
-            height={828}
-            priority={true}
-          />
+          {aspectRatio < 1.3 ? (
+            <Image
+              src="/images/hero--steve-johnson-portrait.jpg"
+              alt="Generative Illustration by Steve Johnson on Unsplash"
+              width={720}
+              height={1080}
+              layout="responsive"
+              priority={true}
+            />
+          ) : (
+            <Image
+              src="/images/hero--steve-johnson-landscape.jpg"
+              alt="Generative Illustration by Steve Johnson on Unsplash"
+              width={900}
+              height={600}
+              layout="responsive"
+              priority={true}
+            />
+          )}
         </div>
       </section>
       <section className="chat__panel">
