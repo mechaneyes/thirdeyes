eb2250e21b706290562cf3d169efedaa8e6f0bdb
mechaneyes <ray@mechaneyes.com>
2023-12-16 16:21:27 -0800

    prisma db wired up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
+import React from "react";
 import type { Metadata } from "next";
-import Provider from "@/app/_context/client-provider"
-import { getServerSession } from "next-auth/next"
-import { authOptions } from "@/app/api/auth/[...nextauth]/route"
+import Provider from "@/app/_context/client-provider";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import "./styles/styles.scss";
@@ -15,15 +16,19 @@ export const metadata: Metadata = {
 
 export default async function RootLayout({
   children,
+  pageProps,
 }: {
   children: React.ReactNode;
+  pageProps: any; // Replace 'any' with the actual type of your pageProps
 }) {
   const session = await getServerSession(authOptions);
 
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Provider session={session}>{children}</Provider>
+        <Provider session={session}>
+          {children && React.cloneElement(children, pageProps)}
+        </Provider>
       </body>
     </html>
   );
