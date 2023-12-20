ef799ea42137311e24b3f7c5693c22a8b0cb3f23
mechaneyes <ray@mechaneyes.com>
2023-12-19 23:44:37 -0800

    added header to show logged in user

--- src/graphql/builder.ts
+++ src/graphql/builder.ts
@@ -1,7 +1,7 @@
 import SchemaBuilder from "@pothos/core";
 import PrismaPlugin from '@pothos/plugin-prisma';
 import type PrismaTypes from '@pothos/plugin-prisma/generated';
-import prisma from "../../../lib/prisma";
+import prisma from "../lib/prisma";
 import { createContext } from './context'
 
 export const builder = new SchemaBuilder<{
