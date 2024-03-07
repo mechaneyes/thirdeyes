415801486691ab28a5894dc200805287a1488985
mechaneyes <ray@mechaneyes.com>
2024-03-07 13:42:37 -0500

    redirect to chat page on login

--- src/app/api/auth/[auth0]/route.ts
+++ src/app/api/auth/[auth0]/route.ts
@@ -2,13 +2,13 @@ import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
 
 export const GET = handleAuth({
   login: handleLogin({
-    returnTo: "/profile",
+    returnTo: "/chat",
   }),
   signup: handleLogin({
     authorizationParams: {
       screen_hint: "signup",
     },
-    returnTo: "/profile",
+    returnTo: "/chat",
   }),
 });
 
