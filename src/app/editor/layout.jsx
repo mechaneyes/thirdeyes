830e03f9d14d2f899a02f7a4031c4c522042a42e
mechaneyes <ray@mechaneyes.com>
2024-03-09 17:45:44 -0500

    added chat + sidebar to editor page
    ° now 2 columns: chat + sidebar left, editor right
    ° did some styling
    ° sidebar links to the typical chat pages. want to integrate it properly
    ° added more text to work with in tiptap

--- src/app/editor/layout.jsx
+++ src/app/editor/layout.jsx
@@ -1,19 +1,34 @@
 "use client";
 
 import dynamic from "next/dynamic";
-import { Grid } from "@carbon/react";
+import { Column, Grid } from "@carbon/react";
 
 const Header = dynamic(() => import("@/app/components/Header"), { ssr: false });
 const Chat = dynamic(() => import("@/app/components/chat"), { ssr: false });
+const Sidebar = dynamic(() => import("@/app/components/sidebar"), {
+  ssr: false,
+});
 
 export default function ChatLayout({ children }) {
   return (
     <div>
       <main>
         <Header />
-        <Grid className="thirdeyes chat">
-          <Chat />
-          {children}
+        <Grid className="thirdeyes chat editor">
+          <Column max={7} xlg={7} lg={7} md={7} sm={4} className="chat__panel">
+            <Sidebar />
+            <Chat />
+          </Column>
+          <Column
+            max={9}
+            xlg={9}
+            lg={9}
+            md={9}
+            sm={4}
+            className="editor__panel"
+          >
+            {children}
+          </Column>
         </Grid>
       </main>
     </div>
