0680f319cb86384722ab25bfe325188de7687eb7
mechaneyes <ray@mechaneyes.com>
2024-03-10 10:27:50 -0400

    built TiptapDiv extension to create div wrapping content

--- src/app/editor/layout.jsx
+++ src/app/editor/layout.jsx
@@ -15,15 +15,15 @@ export default function ChatLayout({ children }) {
       <main>
         <Header />
         <Grid className="thirdeyes chat editor">
-          <Column max={7} xlg={7} lg={7} md={7} sm={4} className="chat__panel">
+          <Column max={9} xlg={9} lg={9} md={9} sm={4} className="chat__panel">
             <SidebarEditor />
             <Chat />
           </Column>
           <Column
-            max={9}
-            xlg={9}
-            lg={9}
-            md={9}
+            max={7}
+            xlg={7}
+            lg={7}
+            md={7}
             sm={4}
             className="editor__panel"
           >
