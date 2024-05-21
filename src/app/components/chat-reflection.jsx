edbd29865fb73c15cd9d64157466b470e5d07540
mechaneyes <ray@mechaneyes.com>
2024-05-21 10:04:18 -0400

    renamed messages-research --> messages-reflection

--- src/app/components/chat-reflection.jsx
+++ src/app/components/chat-reflection.jsx
@@ -6,7 +6,7 @@ import { useUser } from "@auth0/nextjs-auth0/client";
 import { useAtom, useAtomValue } from "jotai";
 import { isLoggedInAtom } from "@/app/store/atoms";
 
-import MessagesResearch from "./messages-research";
+import MessagesResearch from "./messages-reflection";
 import ChatLogin from "./chat-login";
 
 export default function ChatResearch() {
