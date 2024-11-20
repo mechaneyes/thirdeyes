51634bc2174612f1c8556d1d16e569e741ec0189
Ray Weitzenberg <ray@mechaneyes.com>
2024-11-20 02:30:30 +0100

    Update Header.js and button-research.tsx
    - Refactored Header.js: trello + login + thirdbrige tag line
    - Updated button-research.tsx colors and hover effects

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,11 +1,18 @@
+import Link from "next/link";
+
 const Header = () => {
   return (
     <div className="third-header w-full flex flex-row items-center justify-between max-w-7xl py-4 text-4xl text-royalblue font-bd-colonius">
-      Thirdeyes
-      <div className="flex items-center justify-end gap-2 font-mr-eaves-xl-san-ot text-xl font-normal">
-        Third Bridge Creative
-        <b className="text-3xl">·</b>
-        Login
+      <div className="flex items-center justify-end gap-5">
+        <Link href="/">Thirdeyes</Link>
+        <div className="font-mr-eaves-xl-san-ot text-base font-normal">
+          Third Bridge Creative
+        </div>
+      </div>
+      <div className="flex items-center justify-end gap-2 font-mr-eaves-xl-san-ot text-base font-normal">
+        <Link href="https://trello.com/b/eLCNLMP5/ai-development">Trello</Link>
+        <b className="text-xl">·</b>
+        <Link href="#">Login</Link>
       </div>
     </div>
   );
