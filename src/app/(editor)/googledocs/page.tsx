76b632047f86b3612333788b0549ff87f2ce2a5f
Ray Weitzenberg <ray@mechaneyes.com>
2024-11-29 23:09:36 -0500

    more component pruning

--- src/app/(editor)/googledocs/page.tsx
+++ src/app/(editor)/googledocs/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import dynamic from "next/dynamic";
-import GoogleLogin from "@/app/components/google-login";
+import GoogleLogin from "@/app/components/purgatory/google-login";
 
 export default function GoogleDocs() {
   return <GoogleLogin />;
