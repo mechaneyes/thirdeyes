60f5b02697c7c654a44bf027d3572411677a5594
Ray Weitzenberg <ray@mechaneyes.com>
2024-11-29 23:24:53 -0500

    more deletions

--- src/app/lib/tool-wikipedia-query-run.js
+++
@@ -1,6 +0,0 @@
-import { WikipediaQueryRun } from "@langchain/community/tools/wikipedia_query_run";
-
-export const toolWikipediaQueryRun = new WikipediaQueryRun({
-  topKResults: 3,
-  maxDocContentLength: 4000,
-});
