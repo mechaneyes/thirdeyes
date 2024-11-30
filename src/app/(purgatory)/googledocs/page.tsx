ddcdff77d8ade47d4850704f933b71768fdb1c8d
Ray Weitzenberg <ray@mechaneyes.com>
2024-11-30 01:48:13 -0500

    cleanup: npm packages, stray files

--- src/app/(purgatory)/googledocs/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import dynamic from "next/dynamic";
-import GoogleLogin from "@/app/components/purgatory/google-login";
-
-export default function GoogleDocs() {
-  return <GoogleLogin />;
-}
