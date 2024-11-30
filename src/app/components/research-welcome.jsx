fc5557eaa6afc7c9b1fffb473c6afb7fd5453759
Ray Weitzenberg <ray@mechaneyes.com>
2024-11-29 22:32:37 -0500

    copy edits to research/welcome

--- src/app/components/research-welcome.jsx
+++ src/app/components/research-welcome.jsx
@@ -7,12 +7,12 @@ const ResearchWelcome = () => {
   const [reInfluences] = useAtom(researchInfluencesAtom);
 
   return (
-    <div className="research-inner relative h-full pr-4 text-base text-darkslateblue-200 leading-6 whitespace-pre-wrap">
+    <div className="research-inner relative h-full w-11/12 pr-4 text-base text-darkslateblue-200 leading-6 whitespace-pre-wrap">
       <h3 className="pb-1 text-xl text-darkslateblue-300 font-normal">
         Research
       </h3>
       <p>
-        Welcome to the research tool! You can use this tool to find information
+        Welcome to Thirdeyes Research! You can use this tool to find information
         about artists, bands, or other topics.
       </p>
       <p>
@@ -23,7 +23,10 @@ const ResearchWelcome = () => {
         Behind the scenes more information is being fetched about the artist. As
         it becomes available, that info can be accessed using the buttons below.
       </p>
-      <p>More on the way.</p>
+      <p>
+        To get started, enter an artist name in the form in the Strategies panel
+        on the left.
+      </p>
     </div>
   );
 };
