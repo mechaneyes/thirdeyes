edbac0f15f05a71617b6388b62d2630aa90e4dab
Ray Weitzenberg <ray@mechaneyes.com>
2024-12-01 23:59:51 -0500

    more minor header tweaks

--- src/app/components/header.jsx
+++ src/app/components/header.jsx
@@ -2,14 +2,14 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <div className="third-header w-full flex flex-row items-center justify-between max-w-7xl py-4 text-4xl text-royalblue">
+    <div className="third-header w-full flex flex-row items-center justify-between max-w-7xl py-4 text-base font-normal text-[#0062fe]">
       <div className="flex items-center justify-end gap-5">
-        <Link className="font-logo font-normal" href="/">
+        <Link className="font-logo text-4xl" href="/">
           Thirdeyes
         </Link>
         <div className="font-body text-base font-normal"></div>
       </div>
-      <div className="flex items-center justify-end gap-2 font-body text-base font-normal">
+      <div className="flex items-center justify-end gap-2 font-body">
         Third Bridge Creative
         {/* <b className="text-xl">·</b>
         <Link href="#">Login</Link> */}
